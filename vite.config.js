611319307545eeae5c3a7f2a6da6d7220688d239
Aryan Sharma <aryansharma24106@gmail.com>
2025-12-21 20:38:58 +0530

    configure github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/The-Typist/",
   resolve:{
     alias:{
       "@": path.resolve(__dirname,"./src"),
