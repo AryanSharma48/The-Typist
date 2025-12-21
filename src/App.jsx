37255e9087aaf9f99264423a4c03bc2badb7e57a
Aryan Sharma <aryansharma24106@gmail.com>
2025-12-21 20:32:45 +0530

    add tailwind and shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./components/header"
 import Main from "./components/main"
+import ClickSpark from "./components/ClickSpark"
 
 export default function App(){
     return(
-        <>
+        <>  <ClickSpark
+                sparkColor="#fff"
+                sparkSize={10}
+                sparkRadius={25}
+                sparkCount={8}
+                duration={400}
+            >
             <Header />
             <Main />
+            </ClickSpark>
         </>
     )
 }
